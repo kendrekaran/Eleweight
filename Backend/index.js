0853d7533d05602fca6dd87a6085705e37441550
Karan kendre <karankendreg@gmail.com>
2025-02-11 16:43:10 +0530

    Updated Code

--- Backend/index.js
+++ Backend/index.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(cors(
     {
-        origin : [""],
+        origin : ["https://eleweight.vercel.app"],
         methods : ["POST","GET","PUT"],
         credentials : true
     }
